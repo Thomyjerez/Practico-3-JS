5e62a18bc9715dc73f08951adca10f75b974c35f
thomas jerez <tjerez@outlook.com>
2022-07-18 22:17:18 -0300

    agregue el ejercicio 2 sin terminar

---
+++ js/ejercicio2tp3.js
@@ -0,0 +1,16 @@
+
+do{
+    let ciudades = [""];
+    let city = prompt("Ingrese una ciudad");
+   
+    document.write(`<br>` + city);
+    
+    for (let index = 0; index < ciudades.length; index++) {
+        
+       console.log(ciudades)
+    }
+
+}while (confirm("¿Desea ingresar una ciudad?"));
+
+
+  
