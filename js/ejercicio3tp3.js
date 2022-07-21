d256e9dbc8a6499bea903c91655fde2075209648
thomas jerez <tjerez@outlook.com>
2022-07-21 16:56:02 -0300

    termine el ejercicio3

--- js/ejercicio3tp3.js
+++ js/ejercicio3tp3.js
@@ -21,13 +21,18 @@ document.write("Resultado de la suma: " + sumas);
 document.write("<br> Resultado del primer dado: " + pruebaDado1);
 document.write("<br> Resultado del segundo dado: " + pruebaDado2);
 
-document.write(`<table> <tbody><tr><td>Resultado</td><td>Frecuencia</td></tr></tbody>`)
+document.write(`<table> <tbody><tr><td>Resultado</td><td>Frecuencia</td></tr>`)
+
 
 for (let resultado = 2; resultado <= 12; resultado++) {
-  document.write(`<tr><td>${resultado}</td><td>x</td></tr>`);
+  let contador =0;
+  
   for (let posicion = 0; posicion < sumas.length; posicion++) {
     if (resultado === sumas[posicion]) {
       // sumar las apariciones
+      contador++
     }
   }
+  document.write(`<tr><td>${resultado}</td><td>${contador}</td></tr>`)
 }
+document.write(`</tbody></table>`)
