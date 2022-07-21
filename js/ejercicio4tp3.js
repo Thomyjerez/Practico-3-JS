f7e773c69e92c6ecc1e70b625d462fb0ce36a3f5
thomas jerez <tjerez@outlook.com>
2022-07-21 19:45:16 -0300

    agregue el ejercicio 4

--- js/ejercicio4tp3.js
+++ js/ejercicio4tp3.js
@@ -1,1 +1,21 @@
+let numero  = prompt("Introduce un numero entero");
+
+let resultado = parOimpar(Number);
+
+document.write ("El numero " + numero + " es " + resultado);
+
+function parOimpar (numero){
+    
+    if (numero % 2 == 0){
+       
+        return "par";
+    }else{
+        return "impar"
+    }
+}
+
+
+
+
+
 
