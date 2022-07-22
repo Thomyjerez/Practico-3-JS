252b955fc8fbb8a94217310fffb2fc4c8336e280
thomas jerez <tjerez@outlook.com>
2022-07-22 13:41:50 -0300

    termine el ejercicio 2

--- js/ejercicio2tp3.js
+++ js/ejercicio2tp3.js
@@ -1,16 +1,36 @@
+let ciudades = [];
 
 do{
-    let ciudades = [""];
     let city = prompt("Ingrese una ciudad");
-   
-    document.write(`<br>` + city);
-    
-    for (let index = 0; index < ciudades.length; index++) {
-        
-       console.log(ciudades)
-    }
+   ciudades.push(city)
 
-}while (confirm("¿Desea ingresar una ciudad?"));
+}while (confirm("¿Desea ingresar una ciudad?")){
+
+    function ciudad (titulo){
+        document.write(`<h3>`+titulo+`</h3>`)
+        document.write(`<ul>`)
+        for (let i = 0; i < ciudades.length; i++) {
+            document.write(`<li>${ciudades[i]}</li>`)
+              }
+        document.write(`</ul>`)
+    }
+    
+    ciudad ("Ciudades:")
+    document.write(`<br> Longitud del arreglo: ${ciudades.length} `)
+    
+    document.write(`<br> Primera ciudad: ${ciudades[0]}<br/><br> Tercera ciudad: ${ciudades[2]}<br/><br> Ultima ciudad: ${ciudades[3]}<br/>`)
+    ciudades.push("Paris")
+    ciudad("Agrego paris en la ultima posicion")
+    
+    document.write(`<br> Segunda ciudad: ${ciudades[1]}<br/>`)
+    
+    ciudades[1] = "Barcelona"
+    ciudad("Segunda ciudad sustituida por barcelona")
+    
+
+    
+
+}
 
 
   
