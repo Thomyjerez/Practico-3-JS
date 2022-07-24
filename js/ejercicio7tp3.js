1709dd4d64d2e11842457789002df8ea411a14df
thomas jerez <tjerez@outlook.com>
2022-07-24 19:48:07 -0300

    agregue el ejercicio 7

--- js/ejercicio7tp3.js
+++ js/ejercicio7tp3.js
@@ -1,3 +1,24 @@
+let numero=parseInt(prompt("Ingrese el numero para mostrar su tabla de multiplicar"));
+tabla(numero)
+
+function tabla(numero){
+    document.write(`<table>
+    <thead>
+        <tr>
+            <th>Tabla de multiplicar</th>
+        </tr>
+    </thead>
+    <tbody>`);
+
+    for(let i=1; i<=10; i++){
+        document.write(`
+        <tr>
+            <td>${numero} * ${i} </td>
+            <td>${numero * i}</td>
+        </tr>`);  
+    }
+    document.write(`</tbody> </table>`);
+}
 
 
 
